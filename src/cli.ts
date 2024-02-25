bd79f040ac02754cc6da6a9bd0c1424b1cadca93
MuRong <hi@mrong.me>
2024-02-25 10:30:45 +0800

    feat(release): modify version

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 import cac from 'cac'
 import { version } from '../package.json'
-import { build } from './build'
+// import { build } from './build'
 import { release } from './release'
 import { publish } from './publish'
 export function run() {
@@ -8,10 +8,10 @@ export function run() {
   cli.version(version)
 
   cli.option('--dry', 'dry run')
-  cli.command('build', 'build project')
-    .action(() => {
-      build()
-    })
+  // cli.command('build', 'build project')
+  //   .action(() => {
+  //     build()
+  //   })
   cli.command('release', 'release package')
     .action(() => {
       release()
--- src/release.ts
+++ src/release.ts
@@ -1,42 +1,46 @@
 /* eslint-disable no-console */
 import path from 'node:path'
 import fs from 'fs-extra'
-// import { versionBump } from 'bumpp'
+import { versionBump } from 'bumpp'
 import { getConfig } from './config'
-import { runIfNotDry, step } from './utils'
+import { isDryRun, runIfNotDry, step } from './utils'
 
 export async function release() {
   const config = await getConfig()
   const rootDir = process.cwd()
-  // const { version: oldVersion } = fs.readJSONSync(path.join(rootDir, 'package.json'))
-  if (config.release.modifyVersion)
-    step('\n📦Updating package version...')
-    // TODO: bumpp
-    // await runIfNotDry('npx bumpp --no-commit --no-tag --no-push', { cwd: rootDir })
-    // await versionBump({
-    //   commit: false,
-    //   tag: false,
-    //   push: false,
-    // })
+  const { version: oldVersion } = fs.readJSONSync(path.join(rootDir, 'package.json'))
+  if (!isDryRun) {
+    if (config.release.modifyVersion)
+      step('\n📦Updating package version...')
+    await versionBump({
+      commit: false,
+      tag: false,
+      push: false,
+    })
+  }
 
   const { version } = fs.readJSONSync(path.join(rootDir, 'package.json'))
 
-  // if (oldVersion === version && config.release.modifyVersion) {
-  //   console.log('canceled')
-  //   process.exit()
-  // }
+  if (!isDryRun) {
+    if (oldVersion === version && config.release.modifyVersion) {
+      console.log('canceled')
+      process.exit()
+    }
+  }
+
   gitRun(rootDir, version)
 }
 
 async function gitRun(rootDir: string, version: string) {
+  const tag = `v${version}`
   try {
-    await runIfNotDry('git diff', { stdio: 'pipe' })
+    await runIfNotDry('git diff', { stdio: 'pipe', cwd: rootDir })
     step('\n📦Committing changes...')
 
-    await runIfNotDry('git add .', { stdio: 'inherit' })
+    await runIfNotDry('git add .', { stdio: 'inherit', cwd: rootDir })
 
-    await runIfNotDry(`git commit -m "chore: release v${version}"`, { stdio: 'inherit', cwd: rootDir })
-    await runIfNotDry(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit', cwd: rootDir })
+    await runIfNotDry(`git commit -m "chore: release ${tag}"`, { stdio: 'inherit', cwd: rootDir })
+    await runIfNotDry(`git tag -a ${tag} -m "${tag}"`, { stdio: 'inherit', cwd: rootDir })
   }
   catch (error) {
     console.log('No changes to commit.')
